b47adcd04986de7244fa66a3074a128deb3a4e40
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-04 00:09:17 +0100

    allsettled

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,1 +1,15 @@
-#!/usr/bin/node
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((args) => [{
+      status: args[0].status,
+      value: args[0].status === 'fulfilled' ? args[0].value : args[0].reason,
+    },
+    {
+      status: args[1].status,
+      value: args[1].status === 'fulfilled' ? args[1].value : args[1].reason,
+    },
+    ]);
+}

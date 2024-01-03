2556ea5664fc0237a72a95c35d77d05a4bba1c30
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-03 21:39:03 +0100

    advanced

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,5 +1,6 @@
 export default function createIteratorObject(report) {
   let answer = [];
+  /* eslint-disable no-unused-vars */
   for (const [key, val] of Object.entries(report.allEmployees)) {
     answer = [...answer, ...val];
   }

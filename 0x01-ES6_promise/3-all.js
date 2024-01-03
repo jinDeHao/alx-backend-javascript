90476ac0c40ecbc8a893952684389ba2dbbe8f72
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-03 23:30:18 +0100

    all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const photoPromice = uploadPhoto();
+  const userPromice = createUser();
+  return Promise.all([photoPromice, userPromice])
+    .then((args) => { console.log(`${args[0].body} ${args[1].firstName} ${args[1].lastName}`); })
+    .catch(() => { console.log('Signup system offline'); });
+}

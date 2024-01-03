3ec3fee2be0db5ac17b3c85a8c5ab4b706c2befa
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-03 19:07:42 +0100

    value instead of index

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
-export default function appendToEachArrayValue (array, appendString) {
+export default function appendToEachArrayValue(array, appendString) {
   const _array = [];
   for (const idx in array) {
-    _array.push(appendString + idx);
+    _array.push(appendString + array[idx]);
   }
   return _array;
 }

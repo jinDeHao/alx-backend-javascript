b5d90de65dedda29e64dd75d1e5dbb981b8733dd
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-04 00:49:39 +0100

    i promice async is await

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,1 +1,17 @@
-#!/usr/bin/node
+import { uploadPhoto, createUser } from './utils';
+
+const asyncUploadUser = async () => {
+  const data = {};
+  try {
+    data.photo = await uploadPhoto();
+    data.user = await createUser();
+    return (data);
+  } catch (error) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+};
+
+export default asyncUploadUser;

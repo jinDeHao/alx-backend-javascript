065d6e013e2d0f611c157484a2799cb4502d516a
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-03 21:00:33 +0100

    return direct

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: employeesList,
+    getNumberOfDepartments: (emp) => Object.keys(emp).length,
+  };
+}

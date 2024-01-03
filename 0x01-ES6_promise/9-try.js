6dd5c4d263c9bea837c24db316e2f48c381fdce2
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-04 00:36:44 +0100

    9 try to call function

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    queue.push(mathFunction());
+  } catch (error) {
+    queue.push(`${error}`);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}

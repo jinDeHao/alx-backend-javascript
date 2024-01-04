82d25b0edd6048ddddcf7a68ae4d5aa400796e74
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-04 21:50:03 +0100

    class prop

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,1 +1,42 @@
-#!/usr/bin/node
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+  get name() {
+    return this._name;
+  }
+  get length() {
+    return this._length;
+  }
+  get students() {
+    return this._students;
+  }
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+  set length(length) {
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+  }
+  set students(students) {
+    if (Array.isArray(students)) {
+      students.forEach((e) => {
+        if (typeof e !== 'string') {
+          throw new TypeError('Students must be an array of strings');
+        }
+      });
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be an array of strings');
+    }
+  }
+}

d567380cc5b8eebe5cc58e41a9c360cd8a12a8a5
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-04 01:16:11 +0100

    """"""""""""

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,12 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((args) => [
-      args.forEach((element) =>
-      ({
-        status: element.status,
-        value: element.status === 'fulfilled' ? element.value : element.reason
-      }))
-    ]);
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((args) => [
+    args.forEach((element) => ({
+      status: element.status,
+      value: element.status === 'fulfilled' ? element.value : element.reason,
+    })),
+  ]);
 }

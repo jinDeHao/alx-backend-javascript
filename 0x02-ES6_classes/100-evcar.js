dd025754ce0ac540d05bc716966b2fae8a964399
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-04 23:47:17 +0100

    super is obj

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = typeof range === 'string' ? range : undefined;
+  }
+
+  cloneCar() {
+    return new super.constructor();
+  }
+}

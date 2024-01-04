3026cca4bd9c3af7d2c40866becb70945dc60caa
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-04 23:14:59 +0100

    int str

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = typeof size === 'string' ? size : undefined;
+    this._location = typeof location === 'number' ? location : undefined;
+  }
+
+  toString() {
+    return this._location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+}

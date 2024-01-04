5da3e23f977eff11ba63a7bc762f1552163407c6
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-04 23:36:35 +0100

    holly car

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = typeof brand === 'string' ? brand : undefined;
+    this._motor = typeof motor === 'string' ? motor : undefined;
+    this._color = typeof color === 'string' ? color : undefined;
+  }
+
+  cloneCar() {
+    return new this.constructor(this._brand, this._motor, this._color);
+  }
+}

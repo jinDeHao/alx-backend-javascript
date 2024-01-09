0db48675cba2711d479c6f2aea48d63946f0ff32
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-10 00:47:13 +0100

    filter + map + filter

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,22 @@
-#!/usr/bin/node
+const updateStudentGradeByCity = (arrayOfObjects, city, newGrades) => {
+  if (!Array.isArray(arrayOfObjects)) {
+    return [];
+  }
+  return arrayOfObjects
+    .filter((item) => item.location === city)
+    .map((item) => {
+      const help = newGrades.filter((e) => item.id === e.studentId);
+      if (help.length) {
+        return {
+          ...item,
+          grade: help[0].grade,
+        };
+      }
+      return {
+        ...item,
+        grade: 'N/A',
+      };
+    });
+};
+
+export default updateStudentGradeByCity;

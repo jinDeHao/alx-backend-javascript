8f906ba374bc873157ebec67d844c96afe7e1ec0
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-09 17:52:46 +0100

    my first filter method

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,1 +1,6 @@
-#!/usr/bin/node
+export default function getStudentsByLocation(arrayOfObjects, location) {
+  if (!Array.isArray(arrayOfObjects)) {
+    return [];
+  }
+  return arrayOfObjects.filter((item) => item.location === location);
+}

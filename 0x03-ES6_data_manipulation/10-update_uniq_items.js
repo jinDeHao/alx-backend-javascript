824374fef1e6798a28816051394954d39677eb0f
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-10 02:10:54 +0100

    10 updating

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,1 +1,7 @@
-#!/usr/bin/node
+export default function updateUniqueItems(map) {
+  if (map.constructor.name !== 'Map') throw Error('Cannot process');
+  map.forEach((val, key, map) => {
+    if (val === 1) return map.set(key, 100);
+    return null;
+  });
+}

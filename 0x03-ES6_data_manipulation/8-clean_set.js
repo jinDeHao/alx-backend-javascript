5fdbec9fc2f9de17437652ac045bb05fc2d403f1
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-10 02:35:42 +0100

    jovi-aparte-appetit

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+export default function cleanSet(set, startString) {
+  if (
+    !startString ||
+    typeof startString !== 'string' ||
+    set.constructor.name !== 'Set'
+  ) {
+    return '';
+  }
+  const newArray = Array.from(set);
+  return newArray
+    .filter((item) => item.startsWith(startString))
+    .map((item) => item.slice(startString.length))
+    .join('-');
+}

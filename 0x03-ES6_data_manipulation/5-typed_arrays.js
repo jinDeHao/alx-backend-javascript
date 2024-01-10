10a46605eb50a1b584bf0015a2c1417213c194e7
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-10 01:04:41 +0100

    alocation in buffer

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw Error('Position outside range');
+  }
+  const buffer = new ArrayBuffer(length);
+  const dataview = new DataView(buffer);
+  dataview.setInt8(position, value);
+  return dataview;
+}

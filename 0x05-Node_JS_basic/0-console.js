4bef66c1a817b1ae0aa3b60ec4da73fc42849482
jinDeHao <o.idhmaid@gmail.com>
2024-02-27 17:32:56 +0100

    0 + 1

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,1 +1,5 @@
-#!/usr/bin/node
+function displayMessage(str) {
+    console.log(str);
+}
+
+module.exports = displayMessage;
--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    if (process.stdin.isTTY) { process.exit(); }
+})
+if (process.stdin.isTTY === undefined) {
+    process.stdin.on('end', () => {
+        process.stdout.write("This important software is now closing\n");
+    })
+}

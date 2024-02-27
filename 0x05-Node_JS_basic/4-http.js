934e83ee86383d91e128b920d2153e54cbf59ac7
jinDeHao <o.idhmaid@gmail.com>
2024-02-27 20:56:24 +0100

    4 http

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+const http = require('http');
+
+const app = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('Hello Holberton School!');
+});
+app.listen(1245, '127.0.0.1', () => {
+  console.log(`Server running at http://127.0.0.1:1245/`);
+});
+
+module.exports = app;

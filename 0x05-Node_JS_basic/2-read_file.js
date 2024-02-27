c70551ed22c6ea832d696d769f5cc3afa35a9eb7
jinDeHao <o.idhmaid@gmail.com>
2024-02-27 22:21:22 +0100

    tamara

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -5,23 +5,23 @@ const countStudents = (path) => {
   try {
     const csvData = fs.readFileSync(path, 'utf8');
     const lines = csvData.split('\n');
-    rows = lines.filter((item) => item.trim() !== '');
+    const rows = lines.filter((item) => item.trim() !== '');
     console.log(`Number of students: ${rows.length - 1}`);
     const data = [];
     const head = [...rows[0].split(',')];
     rows.splice(0, 1);
-    for (let i = 0; i < rows.length; i++) {
-      let student = {};
+    for (let i = 0; i < rows.length; i += 1) {
+      const student = {};
       let j = 0;
-      head.map((item) => {
+      head.forEach((item) => {
         student[item] = rows[i].split(',')[j];
-        j++;
+        j += 1;
       });
       data.push(student);
     }
     const countField = {};
-    data.map((item) => {
-      if (countField.hasOwnProperty(item.field)) {
+    data.forEach((item) => {
+      if (item.field in countField) {
         countField[item.field] += 1;
       } else {
         countField[item.field] = 1;

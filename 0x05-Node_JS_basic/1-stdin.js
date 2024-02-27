a45e283095a5505d73e4757242cd54b8acd9cf60
jinDeHao <o.idhmaid@gmail.com>
2024-02-27 17:36:07 +0100

    lent

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,10 @@
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
 process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    if (process.stdin.isTTY) { process.exit(); }
+  process.stdout.write(`Your name is: ${data.toString()}`);
+  if (process.stdin.isTTY) { process.exit(); }
 })
 if (process.stdin.isTTY === undefined) {
-    process.stdin.on('end', () => {
-        process.stdout.write("This important software is now closing\n");
-    })
+  process.stdin.on('end', () => {
+    process.stdout.write("This important software is now closing\n");
+  })
 }

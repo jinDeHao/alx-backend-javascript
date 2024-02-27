0be5be3a1cab5dfd0d6499197c3e33c5424e4c8c
jinDeHao <o.idhmaid@gmail.com>
2024-02-27 21:00:07 +0100

    4 + 6

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -6,7 +6,7 @@ const app = http.createServer((request, response) => {
   response.end('Hello Holberton School!');
 });
 app.listen(1245, '127.0.0.1', () => {
-  console.log(`Server running at http://127.0.0.1:1245/`);
+  console.log('Server running at http://127.0.0.1:1245/');
 });
 
 module.exports = app;
--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+  response.send('Hello Holberton School!');
+});
+
+app.listen(1245, () => {
+  console.log('Example app listening on port 1245');
+});
+
+module.exports = app;

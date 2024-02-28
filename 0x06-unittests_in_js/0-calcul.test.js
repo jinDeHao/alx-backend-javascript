7d1927b508de657b819b985fe4dbe6452a57ba4d
jinDeHao <o.idhmaid@gmail.com>
2024-02-28 16:52:39 +0100

    test calculate

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+const assert = require('assert');
+const calculateNumber = require('./0-calcul.js');
+
+describe('my test', () => {
+  it('normal number', () => {
+    assert.equal(calculateNumber(1, 3), 4);
+  });
+  it('radical number', () => {
+    assert.equal(calculateNumber(1, 3.7), 5);
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+    assert.equal(calculateNumber(1.5, 3.7), 6);
+  });
+});

5f4a61e5527900479034965b510fe1d8ee8cba9f
jinDeHao <o.idhmaid@gmail.com>
2024-02-28 21:35:24 +0100

    beforeach - aftereach

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -4,19 +4,21 @@ const sendPaymentRequestToApi = require('./5-payment');
 
 /* eslint-disable */
 describe('payment', () => {
-  it('call sendPaymentRequestToAPI with 100, and 20: ', () => {
-    const consoleLogSpy = sinon.spy(console, 'log');
-    sendPaymentRequestToApi(100, 20);
-    sinon.assert.calledWith(consoleLogSpy, 'The total is: 120');
+  beforeEach(() => {
+    consoleLogSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
     sinon.assert.calledOnce(consoleLogSpy);
     consoleLogSpy.restore();
   });
+  it('call sendPaymentRequestToAPI with 100, and 20: ', () => {
+    sendPaymentRequestToApi(100, 20);
+    sinon.assert.calledWith(consoleLogSpy, 'The total is: 120');
+  });
   it('call sendPaymentRequestToAPI with 10, and 10: ', () => {
-    const consoleLogSpy = sinon.spy(console, 'log');
     sendPaymentRequestToApi(10, 10);
     sinon.assert.calledWith(consoleLogSpy, 'The total is: 20');
-    sinon.assert.calledOnce(consoleLogSpy);
-    consoleLogSpy.restore();
   });
 });
 /* eslint-enable */

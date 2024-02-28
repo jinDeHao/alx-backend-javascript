6a66cab55a718a5b89d05fc925e88de2b3c03142
jinDeHao <o.idhmaid@gmail.com>
2024-02-28 19:41:29 +0100

    4 done

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,1 +1,24 @@
-#!/usr/bin/node
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./4-payment');
+
+/* eslint-disable */
+describe('payment', () => {
+  it('sendPaymentRequestToApi should be the same as Utils.calculateNumber(SUM...)', () => {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(100, 20);
+    sinon.assert.calledWith(calculateNumberSpy, 'SUM', 100, 20);
+    calculateNumberSpy.restore();
+  });
+  it('stubs and let check if it is the same', () => {
+    const consoleLogSpy = sinon.spy(console, 'log');
+    const myStub = sinon.stub().callsFake(() => 10);
+    sinon.replace(Utils, 'calculateNumber', myStub);
+    sendPaymentRequestToApi(100, 20);
+    sinon.assert.calledWith(myStub, 'SUM', 100, 20);
+    sinon.assert.calledWith(consoleLogSpy, 'The total is: 10');
+    sinon.restore();
+    consoleLogSpy.restore();
+  });
+});
+/* eslint-enable */

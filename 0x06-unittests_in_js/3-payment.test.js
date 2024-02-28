f7c654a8590c9c8847d8f625710beb0f8e7a6edf
jinDeHao <o.idhmaid@gmail.com>
2024-02-28 19:23:55 +0100

    3 done

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+/* eslint-disable */
+describe('payment', () => {
+  it('sendPaymentRequestToApi should be the same as Utils.calculateNumber(SUM...)', () => {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(100, 20);
+    sinon.assert.calledWith(calculateNumberSpy, 'SUM', 100, 20);
+    calculateNumberSpy.restore();
+  });
+});
+/* eslint-enable */
--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,1 +1,23 @@
-#!/usr/bin/node
+const { assert } = require('chai');
+
+class Utils {
+  static calculateNumber(type, a, b) {
+    switch (type) {
+      case 'SUM':
+        return Math.round(a) + Math.round(b);
+      case 'SUBTRACT':
+        return Math.round(a) - Math.round(b);
+      case 'DIVIDE':
+        try {
+          assert(Math.round(b) !== 0);
+          return Math.round(a) / Math.round(b);
+        } catch (e) {
+          return 'Error';
+        }
+      default:
+        return 'Error';
+    }
+  }
+}
+
+module.exports = Utils;

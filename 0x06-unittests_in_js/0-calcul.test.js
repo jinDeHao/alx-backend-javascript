ce990ee73fdea117385b6eddb98889797344e437
jinDeHao <o.idhmaid@gmail.com>
2024-02-28 17:18:01 +0100

    non-radical numbers

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -6,10 +6,16 @@ describe('my test', () => {
   it('normal number', () => {
     assert.equal(calculateNumber(1, 3), 4);
   });
+  it('zahlen number', () => {
+    assert.equal(calculateNumber(1, -3), -2);
+  });
   it('radical number', () => {
     assert.equal(calculateNumber(1, 3.7), 5);
     assert.equal(calculateNumber(1.2, 3.7), 5);
     assert.equal(calculateNumber(1.5, 3.7), 6);
+  });
+  it('non-radical number', () => {
+    assert.equal(calculateNumber(Math.E, Math.PI), 6);
   });
 });
 /* eslint-enable */

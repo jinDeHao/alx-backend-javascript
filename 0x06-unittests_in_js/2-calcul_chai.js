bd202ca422912a394eed0a585c1260106959482e
jinDeHao <o.idhmaid@gmail.com>
2024-02-28 17:57:58 +0100

    expect

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,1 +1,21 @@
-#!/usr/bin/node
+const { assert } = require('chai');
+
+const calculateNumber = (type, a, b) => {
+  switch (type) {
+    case 'SUM':
+      return Math.round(a) + Math.round(b);
+    case 'SUBTRACT':
+      return Math.round(a) - Math.round(b);
+    case 'DIVIDE':
+      try {
+        assert(Math.round(b) !== 0);
+        return Math.round(a) / Math.round(b);
+      } catch (e) {
+        return 'Error';
+      }
+    default:
+      return 'Error';
+  }
+};
+
+module.exports = calculateNumber;
--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,1 +1,52 @@
-#!/usr/bin/node
+const calculateNumber = require('./2-calcul_chai');
+const { expect } = require('chai');
+
+/* eslint-disable */
+describe('calculateNumber(SUM)', () => {
+  it('normal number', () => {
+    expect(calculateNumber('SUM', 1, 3)).equal(4);
+  });
+  it('zahlen number', () => {
+    expect(calculateNumber('SUM', 1, -3)).equal(-2);
+  });
+  it('radical number', () => {
+    expect(calculateNumber('SUM', 1, 3.7)).equal(5);
+    expect(calculateNumber('SUM', 1.2, 3.7)).equal(5);
+    expect(calculateNumber('SUM', 1.5, 3.7)).equal(6);
+  });
+  it('non-radical number', () => {
+    expect(calculateNumber('SUM', Math.E, Math.PI)).equal(6);
+  });
+});
+describe('calculateNumber(SUBTRACT)', () => {
+  it('normal number', () => {
+    expect(calculateNumber('SUBTRACT', 1, 3)).equal(-2);
+  });
+  it('zahlen number', () => {
+    expect(calculateNumber('SUBTRACT', 1, -3)).equal(4);
+  });
+  it('radical number', () => {
+    expect(calculateNumber('SUBTRACT', 1.5, 3.7)).equal(-2);
+  });
+  it('non-radical number', () => {
+    expect(calculateNumber('SUBTRACT', Math.E, Math.PI)).equal(0);
+  });
+});
+describe('calculateNumber(DIVIDE)', () => {
+  it('normal number', () => {
+    expect(calculateNumber('DIVIDE', 1, 3)).equal(1 / 3);
+  });
+  it('zahlen number', () => {
+    expect(calculateNumber('DIVIDE', 1, -3)).equal(-1 / 3);
+  });
+  it('radical number', () => {
+    expect(calculateNumber('DIVIDE', 1.5, 3.7)).equal(0.5);
+  });
+  it('non-radical number', () => {
+    expect(calculateNumber('DIVIDE', Math.E, Math.PI)).equal(1);
+  });
+  it('Infinity number', () => {
+    expect(calculateNumber('DIVIDE', 1, 0)).equal('Error');
+  });
+});
+/* eslint-enable */

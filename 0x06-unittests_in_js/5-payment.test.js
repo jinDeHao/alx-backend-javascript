18e01be01d95af83ee66fe3ea06ab8134449b8ff
jinDeHao <o.idhmaid@gmail.com>
2024-02-28 21:31:26 +0100

    5 done

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,1 +1,22 @@
-#!/usr/bin/node
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./5-payment');
+
+/* eslint-disable */
+describe('payment', () => {
+  it('call sendPaymentRequestToAPI with 100, and 20: ', () => {
+    const consoleLogSpy = sinon.spy(console, 'log');
+    sendPaymentRequestToApi(100, 20);
+    sinon.assert.calledWith(consoleLogSpy, 'The total is: 120');
+    sinon.assert.calledOnce(consoleLogSpy);
+    consoleLogSpy.restore();
+  });
+  it('call sendPaymentRequestToAPI with 10, and 10: ', () => {
+    const consoleLogSpy = sinon.spy(console, 'log');
+    sendPaymentRequestToApi(10, 10);
+    sinon.assert.calledWith(consoleLogSpy, 'The total is: 20');
+    sinon.assert.calledOnce(consoleLogSpy);
+    consoleLogSpy.restore();
+  });
+});
+/* eslint-enable */

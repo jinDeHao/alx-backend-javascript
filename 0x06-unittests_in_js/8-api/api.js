d7cef20594f3cf8b3c6a147dc7f565fd7a572f0f
jinDeHao <o.idhmaid@gmail.com>
2024-02-28 23:37:31 +0100

    8

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+  response.send('Welcome to the payment system');
+});
+
+app.listen(7865, () => {
+  console.log('API available on localhost port 7865');
+});
+
+module.exports = app;
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+const { expect } = require('chai');
+const request = require('request');
+
+/* eslint-disable */
+describe('Index page', () => {
+  it('check response status code', (done) => {
+    request.get('http://localhost:7865/', (error, res, body) => {
+      expect(res.status).equal(200);
+      expect(body).equal('Welcome to the payment system');
+    });
+    done();
+  });
+});
+/* eslint-enable */

d90946d2d3f447e79503e06b5f7e68ee3c533992
jinDeHao <o.idhmaid@gmail.com>
2024-02-28 23:51:11 +0100

    api test

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -3,10 +3,15 @@ const request = require('request');
 
 /* eslint-disable */
 describe('Index page', () => {
+  it('check response body', (done) => {
+    request.get('http://localhost:7865/', (error, res, body) => {
+      expect(body).equal('Welcome to the payment system');
+    });
+    done();
+  });
   it('check response status code', (done) => {
     request.get('http://localhost:7865/', (error, res, body) => {
-      expect(res.status).equal(200);
-      expect(body).equal('Welcome to the payment system');
+      expect(res.statusCode).equal(200);
     });
     done();
   });

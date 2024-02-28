1b7f14e96c8223bb6b1ac870b0c5698bc60e5da7
jinDeHao <o.idhmaid@gmail.com>
2024-02-29 00:37:45 +0100

    9 done

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,1 +1,17 @@
-#!/usr/bin/node
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+  response.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id(\\d+)', (request, response) => {
+  response.send(`Payment methods for cart ${request.params.id}`);
+});
+
+app.listen(7865, () => {
+  console.log('API available on localhost port 7865');
+});
+
+module.exports = app;
--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,1 +1,55 @@
-#!/usr/bin/node
+const { expect, assert } = require('chai');
+const request = require('request');
+
+/* eslint-disable */
+describe('Index page', () => {
+  it('check response body', (done) => {
+    request.get('http://localhost:7865/', (error, res, body) => {
+      expect(body).equal('Welcome to the payment system');
+    });
+    done();
+  });
+  it('check response status code invalid endpoint', (done) => {
+    request.get('http://localhost:7865/DFG', (error, res, body) => {
+      expect(res.statusCode).equal(404);
+    });
+    done();
+  });
+  it('check response status code', (done) => {
+    request.get('http://localhost:7865/', (error, res, body) => {
+      expect(res.statusCode).equal(200);
+    });
+    done();
+  });
+  it('check response Error', (done) => {
+    request.get('http://localhost:7865/', (error, res, body) => {
+      assert.isNull(error);
+    });
+    done();
+  });
+});
+
+describe('cart page', () => {
+  it('check status code and body', (done) => {
+    request.get('http://localhost:7865/cart/123', (error, response, body) => {
+      expect(response.statusCode).equal(200);
+      expect(body).equal('Payment methods for cart 123');
+      done();
+    });
+  });
+  it('should respond with 404 Not Found when id is not number', (done) => {
+    request.get('http://localhost:7865/cart/abc', (error, response, body) => {
+      expect(response.statusCode).equal(404);
+      done();
+    });
+  });
+
+  it('should respond with 404 not found when no id is provided', (done) => {
+    request.get('http://localhost:7865/cart/', (error, response, body) => {
+      expect(response.statusCode).equal(404);
+      done();
+    });
+  });
+});
+
+/* eslint-enable */

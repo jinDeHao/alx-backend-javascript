514620e3adec43cd366cebd3b5021f0e929787a3
jinDeHao <o.idhmaid@gmail.com>
2024-02-29 01:22:39 +0100

    done 10

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,1 +1,35 @@
-#!/usr/bin/node
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (request, response) => {
+  response.send('Welcome to the payment system');
+});
+
+app.post('/login', (request, response) => {
+  const userName = request.body.userName;
+  response.send(`Welcome ${userName}`);
+});
+
+app.get('/available_payments', (request, response) => {
+  response.send(
+    JSON.stringify({
+      payment_methods: {
+        credit_cards: true,
+        paypal: false,
+      },
+    })
+  );
+});
+
+app.get('/cart/:id(\\d+)', (request, response) => {
+  response.send(`Payment methods for cart ${request.params.id}`);
+});
+
+app.listen(7865, () => {
+  console.log('API available on localhost port 7865');
+});
+
+module.exports = app;
--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,1 +1,118 @@
-#!/usr/bin/node
+const { expect, assert } = require('chai');
+const request = require('request');
+
+/* eslint-disable */
+describe('Index page', () => {
+  it('check response body', (done) => {
+    request.get('http://localhost:7865/', (error, res, body) => {
+      expect(body).equal('Welcome to the payment system');
+    });
+    done();
+  });
+  it('check response status code invalid endpoint', (done) => {
+    request.get('http://localhost:7865/DFG', (error, res, body) => {
+      expect(res.statusCode).equal(404);
+    });
+    done();
+  });
+  it('check response status code', (done) => {
+    request.get('http://localhost:7865/', (error, res, body) => {
+      expect(res.statusCode).equal(200);
+    });
+    done();
+  });
+  it('check response Error', (done) => {
+    request.get('http://localhost:7865/', (error, res, body) => {
+      assert.isNull(error);
+    });
+    done();
+  });
+});
+
+describe('cart page', () => {
+  it('check status code and body', (done) => {
+    request.get('http://localhost:7865/cart/123', (error, response, body) => {
+      expect(response.statusCode).equal(200);
+      expect(body).equal('Payment methods for cart 123');
+      done();
+    });
+  });
+  it('should respond with 404 Not Found when id is not number', (done) => {
+    request.get('http://localhost:7865/cart/abc', (error, response, body) => {
+      expect(response.statusCode).equal(404);
+      done();
+    });
+  });
+
+  it('should respond with 404 not found when no id is provided', (done) => {
+    request.get('http://localhost:7865/cart/', (error, response, body) => {
+      expect(response.statusCode).equal(404);
+      done();
+    });
+  });
+});
+
+describe('available_payments', () => {
+  it('check status code and body', (done) => {
+    request.get(
+      'http://localhost:7865/available_payments',
+      (error, response, body) => {
+        expect(response.statusCode).equal(200);
+        assert.deepEqual(JSON.parse(body), {
+          payment_methods: {
+            credit_cards: true,
+            paypal: false,
+          },
+        });
+        done();
+      }
+    );
+  });
+  it('should respond with 404 Not Found when id is not number', (done) => {
+    request.get(
+      'http://localhost:7865/available_payments/abc',
+      (error, response, body) => {
+        expect(response.statusCode).equal(404);
+        done();
+      }
+    );
+  });
+});
+
+describe('login', () => {
+  it('check status code and body', (done) => {
+    request.post(
+      {
+        url: 'http://localhost:7865/login',
+        json: true,
+        body: { userName: 'Betty' },
+      },
+      (error, response, body) => {
+        expect(response.statusCode).equal(200);
+        expect(body).equal('Welcome Betty');
+        done();
+      }
+    );
+  });
+  // it('check status code and body when send bad data', (done) => {
+  //   request.post(
+  //     {
+  //       url: 'http://localhost:7865/login',
+  //       json: true,
+  //       body: { age: 13 },
+  //     },
+  //     (error, response, body) => {
+  //       expect(response.statusCode).equal(400);
+  //       done();
+  //     }
+  //   );
+  // });
+  it('should respond with 404 Not Found when it login with get', (done) => {
+    request.get('http://localhost:7865/login', (error, response, body) => {
+      expect(response.statusCode).equal(404);
+      done();
+    });
+  });
+});
+
+/* eslint-enable */
